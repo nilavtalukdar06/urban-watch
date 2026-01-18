6515aec8dc22274db37ba996b4ca26cb3f8877e3
Web <webd75369@gmail.com>
2026-01-18 21:35:52 +0530

    added onboarding form component

--- apps/web/app/onboarding/page.tsx
+++ apps/web/app/onboarding/page.tsx
@@ -1,4 +1,5 @@
 import { AuthGuard } from "@/modules/auth/components/auth-guard";
+import { OnboardingForm } from "@/modules/profile/components/onboarding-form";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
@@ -12,8 +13,14 @@ export default async function Onboarding() {
   }
   return (
     <AuthGuard>
-      <div className="p-4">
-        <p className="text-muted-foreground font-light">Onboarding</p>
+      <div className="p-4 min-h-svh h-full w-full flex flex-col gap-y-2 justify-center items-center max-w-md mx-auto">
+        <h2 className="text-neutral-700 font-medium text-lg md:text-xl">
+          Complete Your Profile
+        </h2>
+        <p className="text-sm md:text-base font-normal text-neutral-500">
+          Please fill in all the details correctly
+        </p>
+        <OnboardingForm />
       </div>
     </AuthGuard>
   );
---
+++ apps/web/modules/profile/components/onboarding-form.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export function OnboardingForm() {
+  return (
+    <div className="py-2 w-full">
+      <p>Onboarding Form</p>
+    </div>
+  );
+}
