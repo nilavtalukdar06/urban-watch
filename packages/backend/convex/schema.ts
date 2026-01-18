d5e3134872deecdfe8765db3ca53c709321a5cc6
Web <webd75369@gmail.com>
2026-01-18 20:25:17 +0530

    added user schema

---
+++ packages/backend/convex/schema.ts
@@ -0,0 +1,17 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  citizens: defineTable({
+    fullName: v.string(),
+    dateOfBirth: v.string(),
+    phoneNumber: v.string(),
+    permanentAddress: v.string(),
+    userId: v.string(),
+  }).index("by_userId", ["userId"]),
+  userIdentity: defineTable({
+    isAuthorized: v.boolean(),
+    documentType: v.optional(v.string()),
+    citizenId: v.id("citizens"),
+  }).index("by_citizenId", ["citizenId"]),
+});
