791ab6c5d3f68baeb9c210f4a487718480426dfd
Web <webd75369@gmail.com>
2026-01-20 13:24:33 +0530

    added preloading

--- apps/web/app/(dashboard)/chatbot/page.tsx
+++ apps/web/app/(dashboard)/chatbot/page.tsx
@@ -1,13 +1,23 @@
 import { Navbar } from "@/components/shared/navbar";
 import { PromptProvider } from "@/modules/chatbot/context/prompt-provider";
 import { MessageContainer } from "@/modules/chatbot/views/message-container";
+import { preloadQuery } from "convex/nextjs";
+import { api } from "@workspace/backend/convex/_generated/api";
+import { auth } from "@clerk/nextjs/server";
 
-export default function Chatbot() {
+export default async function Chatbot() {
+  const token =
+    (await (await auth()).getToken({ template: "convex" })) ?? undefined;
+  const result = await preloadQuery(
+    api.functions.chatbot.getMessages,
+    {},
+    { token },
+  );
   return (
     <PromptProvider>
       <div className="h-screen w-full flex flex-col">
         <Navbar />
-        <MessageContainer />
+        <MessageContainer preloadedMessages={result} />
       </div>
     </PromptProvider>
   );
--- apps/web/modules/chatbot/views/message-container.tsx
+++ apps/web/modules/chatbot/views/message-container.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { MessageSquare } from "lucide-react";
 import { PromptInput } from "../components/prompt-input";
 import {
@@ -6,8 +8,13 @@ import {
   ConversationEmptyState,
   ConversationScrollButton,
 } from "@workspace/ui/components/ai-elements/conversation";
+import { Preloaded, usePreloadedQuery } from "convex/react";
+import { api } from "@workspace/backend/convex/_generated/api";
 
-export function MessageContainer() {
+export function MessageContainer(props: {
+  preloadedMessages: Preloaded<typeof api.functions.chatbot.getMessages>;
+}) {
+  const messages = usePreloadedQuery(props.preloadedMessages);
   return (
     <section className="flex-1 flex flex-col h-full p-4">
       <Conversation className="flex flex-col flex-1 min-h-0">
