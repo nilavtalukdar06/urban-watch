d1a28e0d8194ce238ee81b98ce6adf3bbc83b1be
Web <webd75369@gmail.com>
2026-01-21 13:51:06 +0530

    updated user button

--- apps/web/components/shared/navbar.tsx
+++ apps/web/components/shared/navbar.tsx
@@ -2,7 +2,6 @@
 
 import { DeleteMessages } from "@/modules/chatbot/components/delete-messages";
 import { UserButton } from "@clerk/nextjs";
-import { MessageSquareCodeIcon, TriangleAlertIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -16,20 +15,7 @@ export function Navbar() {
       </Link>
       <nav className="flex justify-end gap-x-3 items-center">
         {pathname === "/chatbot" && <DeleteMessages />}
-        <UserButton>
-          <UserButton.MenuItems>
-            <UserButton.Link
-              label="Chat with AI"
-              href="/chatbot"
-              labelIcon={<MessageSquareCodeIcon size={16} />}
-            />
-            <UserButton.Link
-              label="My Reports"
-              href="/reports"
-              labelIcon={<TriangleAlertIcon size={16} />}
-            />
-          </UserButton.MenuItems>
-        </UserButton>
+        <UserButton />
       </nav>
     </header>
   );
