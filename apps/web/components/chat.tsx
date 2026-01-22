b3d9fbec2aba828512787edf64d99dfd37c103ff
Web <webd75369@gmail.com>
2026-01-23 02:01:17 +0530

    minor code updates

--- apps/web/components/chat.tsx
+++ apps/web/components/chat.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import type { User, Channel as StreamChannel } from "stream-chat";
 import {
   useCreateChatClient,
@@ -12,6 +12,7 @@ import {
   Thread,
   Window,
 } from "stream-chat-react";
+import { v4 as uuidv4 } from "uuid";
 
 interface Props {
   otherUserId: string;
@@ -24,9 +25,44 @@ interface Props {
 }
 
 export function ChatComponent(props: Props) {
+  const [channel, setChannel] = useState<StreamChannel>();
+  const client = useCreateChatClient({
+    apiKey: process.env.NEXT_PUBLIC_STREAM_API_KEY!,
+    tokenOrProvider: props.token,
+    userData: props.user as User,
+  });
+
+  useEffect(() => {
+    if (!client) {
+      return;
+    }
+    const channel = client.channel("messaging", uuidv4(), {
+      members: [props.user.id, props.otherUserId],
+    });
+    setChannel(channel);
+  }, [client]);
+
+  if (!client) {
+    return (
+      <div className="min-h-svh w-full h-full flex justify-center items-center p-4">
+        <p className="text-lg text-muted-foreground font-light animate-pulse">
+          Setting up client and connection...
+        </p>
+      </div>
+    );
+  }
   return (
     <div className="w-full">
-      <p className="text-muted-foreground font-light">Chat Now</p>
+      <Chat client={client}>
+        <Channel channel={channel}>
+          <Window>
+            <ChannelHeader />
+            <MessageList />
+            <MessageInput />
+          </Window>
+          <Thread />
+        </Channel>
+      </Chat>
     </div>
   );
 }
