917314be6d29c3ca316a9dc5f6d7ef2d37b436a0
Web <webd75369@gmail.com>
2026-01-24 21:00:50 +0530

    created upload file component

---
+++ apps/web/modules/profile/views/upload-file.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export function UploadFile() {
+  return (
+    <div className="max-w-xs w-full mx-auto my-24">
+      <p className="text-muted-foreground font-light text-center">
+        Upload File
+      </p>
+    </div>
+  );
+}
