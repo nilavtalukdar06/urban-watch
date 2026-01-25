1e75578353909f2ea340c87f7d846abae1d5841f
Web <webd75369@gmail.com>
2026-01-25 22:37:54 +0530

    added organization onboarding form id

--- apps/admin/modules/profile/components/onboarding-form.tsx
+++ apps/admin/modules/profile/components/onboarding-form.tsx
@@ -1,11 +1,99 @@
 "use client";
 
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@workspace/ui/components/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@workspace/ui/components/form";
+import { Input } from "@workspace/ui/components/input";
+import { Textarea } from "@workspace/ui/components/textarea";
+
+const formSchema = z.object({
+  name: z.string().min(2, { message: "Name is too short" }),
+  purpose: z.string().min(5, { message: "Purpose is too short" }),
+  goal: z.string().min(5, { message: "Goal is too short" }),
+});
+
 export function OnboardingForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      purpose: "",
+      goal: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
   return (
-    <div className="w-full">
-      <p className="text-center text-muted-foreground font-light">
-        Onboarding Form
-      </p>
+    <div className="w-full my-2">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="font-normal text-neutral-700">
+                  Organization Name
+                </FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter the organization name" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="purpose"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="font-normal text-neutral-700">
+                  Purpose of organization
+                </FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Enter the purpose of your organization"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="goal"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="font-normal text-neutral-700">
+                  Goal of organization
+                </FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Enter the goals of your organization"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="w-full flex justify-center items-center">
+            <Button className="w-fit">Submit</Button>
+          </div>
+        </form>
+      </Form>
     </div>
   );
 }
