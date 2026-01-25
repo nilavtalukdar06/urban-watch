b33087e2a2f299040b9acaec90aeebed7a3c5597
Web <webd75369@gmail.com>
2026-01-25 11:14:20 +0530

    added resend as an internal package and added a function to send email

---
+++ packages/emails/src/verify-account.ts
@@ -0,0 +1,22 @@
+import { resend } from "./lib/resend.js";
+
+export async function verificationEmail(
+  emailTo: string,
+  subject: string,
+  body: string,
+) {
+  const { data, error } = await resend.emails.send({
+    from: "Urban Watch <nilavtalukdar06@imagify.space>",
+    to: [emailTo],
+    subject,
+    html: `<p>${body}</p>`,
+  });
+  if (error) {
+    return {
+      success: false,
+      message: error.message,
+    };
+  } else {
+    return data;
+  }
+}
