f1f689e12f124d38f3a04efad74d1372d21cf66c
Web <webd75369@gmail.com>
2026-01-25 22:49:07 +0530

    added function to update metadata

---
+++ apps/admin/modules/profile/functions/update-metadata.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import { auth, clerkClient } from "@clerk/nextjs/server";
+
+export const updateMetadata = async () => {
+  const { orgId } = await auth();
+  if (!orgId) {
+    throw new Error("organization id is not present");
+  }
+  const client = await clerkClient();
+  const result = await client.organizations.updateOrganization(orgId, {
+    publicMetadata: {
+      hasProfile: true,
+    },
+  });
+  return result;
+};
