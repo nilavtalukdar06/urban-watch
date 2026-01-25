77a8a88cc503aba9e4c9909b9d35361e13d49ec8
Web <webd75369@gmail.com>
2026-01-25 16:37:19 +0530

    updated verification function

--- packages/backend/convex/functions/verification.ts
+++ packages/backend/convex/functions/verification.ts
@@ -2,9 +2,17 @@ import { mutation } from "../_generated/server";
 import { v } from "convex/values";
 
 export const updateStatus = mutation({
-  args: { id: v.id("userIdentity") },
+  args: { userId: v.id("citizens") },
   handler: async (ctx, args) => {
-    const { id } = args;
+    const { userId } = args;
+    const identity = await ctx.db
+      .query("userIdentity")
+      .filter((q) => q.eq(q.field("citizenId"), userId))
+      .first();
+    if (!identity) {
+      throw new Error("identity not found");
+    }
+    const id = identity._id;
     const result = await ctx.db.patch("userIdentity", id, {
       verificationStatus: "in-review",
     });
