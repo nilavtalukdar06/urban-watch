955885ef4e80f04f8c7330bacb63d96d21ac4e67
Web <webd75369@gmail.com>
2026-01-25 22:21:25 +0530

    created onboarding layout

--- apps/admin/app/onboarding/page.tsx
+++ apps/admin/app/onboarding/page.tsx
@@ -1,3 +1,4 @@
+import { OnboardingForm } from "@/modules/profile/components/onboarding-form";
 import { auth, clerkClient } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
@@ -14,8 +15,18 @@ export default async function OnboardingPage() {
     redirect("/");
   }
   return (
-    <div className="p-4">
-      <p className="text-muted-foreground font-light">Onboarding Page</p>
+    <div className="p-4 min-h-svh w-full h-full gap-2 flex flex-col justify-center items-center">
+      <div className="max-w-sm mx-auto w-full flex flex-col gap-4">
+        <div className="w-full text-center gap-2">
+          <h3 className="text-lg text-neutral-700">
+            Complete Organization Profile
+          </h3>
+          <p className="text-sm font-light text-muted-foreground">
+            Please fill in all the details correctly to continue
+          </p>
+        </div>
+        <OnboardingForm />
+      </div>
     </div>
   );
 }
---
+++ apps/admin/modules/profile/components/onboarding-form.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export function OnboardingForm() {
+  return (
+    <div className="w-full">
+      <p className="text-center text-muted-foreground font-light">
+        Onboarding Form
+      </p>
+    </div>
+  );
+}
