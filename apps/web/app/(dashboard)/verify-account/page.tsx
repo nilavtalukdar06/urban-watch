75e3bf5638f46487444ebac95475b1f6f9de980c
Web <webd75369@gmail.com>
2026-01-25 15:47:06 +0530

    added verification guards

--- apps/web/app/(dashboard)/verify-account/page.tsx
+++ apps/web/app/(dashboard)/verify-account/page.tsx
@@ -1,7 +1,21 @@
 import { Navbar } from "@/components/shared/navbar";
 import { UploadFile } from "@/modules/profile/views/upload-file";
+import { auth } from "@clerk/nextjs/server";
+import { api } from "@workspace/backend/convex/_generated/api";
+import { fetchQuery } from "convex/nextjs";
+import { redirect } from "next/navigation";
 
-export default function VerifyAccount() {
+export default async function VerifyAccount() {
+  const result = await auth();
+  const token = (await result.getToken({ template: "convex" })) ?? undefined;
+  const status = await fetchQuery(
+    api.functions.users.checkVerificationStatus,
+    {},
+    { token },
+  );
+  if (status?.isAuthorized || status?.verificationStatus === "in-review") {
+    redirect("/");
+  }
   return (
     <div className="w-full">
       <Navbar />
