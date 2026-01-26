ebb40c204e24012e349495db9bbc0ba859892396
Web <webd75369@gmail.com>
2026-01-26 20:26:43 +0530

    only admins can create and assign tasks

--- apps/admin/app/(dashboard)/page.tsx
+++ apps/admin/app/(dashboard)/page.tsx
@@ -1,6 +1,8 @@
 import { CreateTask } from "@/modules/tasks/components/create-task";
+import { auth } from "@clerk/nextjs/server";
 
 export default async function Home() {
+  const { has } = await auth();
   return (
     <div className="px-4 pb-4">
       <h3 className="text-lg text-neutral-600 font-light">
@@ -9,7 +11,7 @@ export default async function Home() {
       <p className="text-muted-foreground font-light text-sm">
         Create and manage all of your tasks here
       </p>
-      <CreateTask />
+      {has({ role: "org:admin" }) && <CreateTask />}
     </div>
   );
 }
