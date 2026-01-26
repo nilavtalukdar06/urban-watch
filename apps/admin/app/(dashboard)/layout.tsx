3fc026f78a0e2c97a6e0fef33a42e6343036264b
Web <webd75369@gmail.com>
2026-01-26 10:41:44 +0530

    added sidebar provider

--- apps/admin/app/(dashboard)/layout.tsx
+++ apps/admin/app/(dashboard)/layout.tsx
@@ -1,6 +1,11 @@
 import { AuthGuard } from "@/modules/auth/components/auth-guard";
 import { auth, clerkClient } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import {
+  SidebarProvider,
+  SidebarTrigger,
+} from "@workspace/ui/components/sidebar";
+import { AppSidebar } from "@/components/shared/app-sidebar";
 
 interface Props {
   children: React.ReactNode;
@@ -18,5 +23,17 @@ export default async function Layout({ children }: Props) {
   if (!organization.publicMetadata?.hasProfile) {
     redirect("/onboarding");
   }
-  return <AuthGuard>{children}</AuthGuard>;
+  return (
+    <AuthGuard>
+      <SidebarProvider>
+        <AppSidebar />
+        <main>
+          <div className="p-2">
+            <SidebarTrigger />
+          </div>
+          {children}
+        </main>
+      </SidebarProvider>
+    </AuthGuard>
+  );
 }
---
+++ apps/admin/components/shared/app-sidebar.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export function AppSidebar() {
+  return (
+    <div className="p-4">
+      <p className="text-muted-foreground">App Sidebar</p>
+    </div>
+  );
+}
