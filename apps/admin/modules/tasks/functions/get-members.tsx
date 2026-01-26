95019ccf443745f7375a3eb10ac99e5a928966df
Web <webd75369@gmail.com>
2026-01-26 19:10:09 +0530

    added list for getting organization members

---
+++ apps/admin/modules/tasks/functions/get-members.tsx
@@ -0,0 +1,24 @@
+"use server";
+
+import { auth, clerkClient } from "@clerk/nextjs/server";
+
+export async function getOrganizationMemebers() {
+  const { userId, orgId } = await auth();
+  if (!userId) {
+    throw new Error("the user is not authenticated");
+  }
+  if (!orgId) {
+    throw new Error("organization doesn't exist");
+  }
+  const members = await (
+    await clerkClient()
+  ).organizations.getOrganizationMembershipList({
+    organizationId: orgId,
+  });
+  const result = members.data.map((member) => ({
+    userId: member.publicUserData?.userId,
+    name:
+      member.publicUserData?.firstName + " " + member.publicUserData?.lastName,
+  }));
+  return result;
+}
