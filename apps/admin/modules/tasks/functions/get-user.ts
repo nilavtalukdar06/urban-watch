51b895c17e471963454f8dc0777436e997bb5b93
Web <webd75369@gmail.com>
2026-01-27 10:21:59 +0530

    added a function to retrieve the user info from clerk user id

---
+++ apps/admin/modules/tasks/functions/get-user.ts
@@ -0,0 +1,16 @@
+"use server";
+
+import { auth, clerkClient } from "@clerk/nextjs/server";
+
+export const getUser = async (assignedToUserId: string) => {
+  const { userId, orgId } = await auth();
+  if (!userId) {
+    throw new Error("the user is not authenticated");
+  }
+  if (!orgId) {
+    throw new Error("organization doesn't exist");
+  }
+  const client = await clerkClient();
+  const user = await client.users.getUser(assignedToUserId);
+  return user;
+};
