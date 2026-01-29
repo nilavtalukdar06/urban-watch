be7cd8dd533130d3ee62e497a68a1607e309f802
Web <webd75369@gmail.com>
2026-01-29 17:11:51 +0530

    added the dialog layout

--- apps/admin/modules/payments/components/enable-payments.tsx
+++ apps/admin/modules/payments/components/enable-payments.tsx
@@ -1,7 +1,16 @@
 "use client";
 
 import { Button } from "@workspace/ui/components/button";
-import { Dialog, DialogTrigger } from "@workspace/ui/components/dialog";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@workspace/ui/components/dialog";
 
 export function EnablePayments() {
   return (
@@ -11,6 +20,33 @@ export function EnablePayments() {
           Enable Payments
         </Button>
       </DialogTrigger>
+      <DialogContent className="w-[425px] rounded-none p-5">
+        <DialogHeader>
+          <DialogTitle className="text-neutral-700 font-light">
+            Enable Payments
+          </DialogTitle>
+          <DialogDescription className="font-light text-sm text-muted-foreground">
+            Go to your stripe dashboard and grab your public and private api
+            keys
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button
+              variant="outline"
+              className="rounded-none shadow-none bg-sidebar border font-normal"
+            >
+              Cancel
+            </Button>
+          </DialogClose>
+          <Button
+            variant="destructive"
+            className="rounded-none shadow-none font-normal"
+          >
+            Save Keys
+          </Button>
+        </DialogFooter>
+      </DialogContent>
     </Dialog>
   );
 }
