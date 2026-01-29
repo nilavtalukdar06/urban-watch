24c73f63e9ea29f60fce2776cf3d504068a8890c
Web <webd75369@gmail.com>
2026-01-29 22:20:38 +0530

    completed the dialog ui

--- apps/admin/modules/payments/components/delete-keys.tsx
+++ apps/admin/modules/payments/components/delete-keys.tsx
@@ -1,6 +1,15 @@
 "use client";
 
-import { Dialog, DialogTrigger } from "@workspace/ui/components/dialog";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@workspace/ui/components/dialog";
 import { Button } from "@workspace/ui/components/button";
 import type { Id } from "@workspace/backend/convex/_generated/dataModel";
 
@@ -16,6 +25,34 @@ export function DeleteKeys({ keyId }: { keyId: Id<"apiKeys"> | undefined }) {
           Delete Keys
         </Button>
       </DialogTrigger>
+      <DialogContent className="w-[425px] rounded-none p-5">
+        <DialogHeader>
+          <DialogTitle className="font-light text-neutral-700">
+            Delete Keys
+          </DialogTitle>
+          <DialogDescription className="font-light text-sm text-muted-foreground">
+            Are you sure that you want to delete your current stripe keys? if
+            you want to add new stripe keys to your account then you must have
+            to delete the old ones.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <DialogClose>
+            <Button
+              variant="outline"
+              className="shadow-none rounded-none bg-sidebar font-normal"
+            >
+              Cancel
+            </Button>
+          </DialogClose>
+          <Button
+            variant="destructive"
+            className="shadow-none rounded-none font-normal"
+          >
+            Delete
+          </Button>
+        </DialogFooter>
+      </DialogContent>
     </Dialog>
   );
 }
