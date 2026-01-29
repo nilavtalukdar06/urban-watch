6742f15b4bf4774436b67dbb3f96278ce05c2569
Web <webd75369@gmail.com>
2026-01-29 19:23:17 +0530

    added an api route to store api keys to infisical

---
+++ apps/admin/app/api/secrets/create/route.ts
@@ -0,0 +1,70 @@
+import { auth } from "@clerk/nextjs/server";
+import { InfisicalSDK } from "@infisical/sdk";
+import { api } from "@workspace/backend/convex/_generated/api";
+import { fetchMutation } from "convex/nextjs";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const requestSchema = z.object({
+  keyName: z.string().min(2),
+  publicKey: z.string().min(5),
+  secretKey: z.string().min(5),
+});
+
+export async function POST(request: NextRequest) {
+  try {
+    const { userId, orgId, getToken } = await auth();
+    if (!userId) {
+      return NextResponse.json(
+        { error: "the user is not authenticated" },
+        { status: 401 },
+      );
+    }
+    if (!orgId) {
+      return NextResponse.json(
+        { error: "organization doesn't exist" },
+        { status: 403 },
+      );
+    }
+    const requestBody = await request.json();
+    const parsedSchema = requestSchema.safeParse(requestBody);
+    if (!parsedSchema.success) {
+      return NextResponse.json(
+        { error: "invalid key format" },
+        { status: 400 },
+      );
+    }
+    const token = await getToken({ template: "convex" });
+    const client = new InfisicalSDK({
+      siteUrl: "https://app.infisical.com",
+    });
+    await client.auth().universalAuth.login({
+      clientId: process.env.MACHINE_ID!,
+      clientSecret: process.env.MACHINE_SECRET!,
+    });
+    const publicKey = await client
+      .secrets()
+      .createSecret(`tenant-public-${orgId}`, {
+        environment: "dev",
+        projectId: process.env.PROJECT_ID!,
+        secretValue: parsedSchema.data.publicKey,
+      });
+    const secretKey = await client
+      .secrets()
+      .createSecret(`tenant-secret-${orgId}`, {
+        environment: "dev",
+        projectId: process.env.PROJECT_ID!,
+        secretValue: parsedSchema.data.secretKey,
+      });
+    return NextResponse.json(
+      { keys: { publicKey, secretKey } },
+      { status: 201 },
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "failed to create secret" },
+      { status: 500 },
+    );
+  }
+}
