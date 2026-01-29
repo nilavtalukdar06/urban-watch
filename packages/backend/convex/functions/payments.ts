750e0a7105822293dcf88f876c1f3e9a268bd413
Web <webd75369@gmail.com>
2026-01-29 19:51:13 +0530

    added mutation function to save api prefixes

--- packages/backend/convex/functions/payments.ts
+++ packages/backend/convex/functions/payments.ts
@@ -1,4 +1,5 @@
-import { query } from "../_generated/server";
+import { v } from "convex/values";
+import { mutation, query } from "../_generated/server";
 
 export const checkPaymentStatus = query({
   args: {},
@@ -15,6 +16,33 @@ export const checkPaymentStatus = query({
       .query("organization")
       .filter((q) => q.eq(q.field("organizationId"), organizationId))
       .first();
+    return result;
+  },
+});
+
+export const saveKeys = mutation({
+  args: {
+    keyName: v.string(),
+    publicKey: v.string(),
+    secretKey: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const auth = await ctx.auth.getUserIdentity();
+    if (!auth) {
+      throw new Error("the user is not authenticated");
+    }
+    const organizationId = auth?.orgId as string;
+    if (!organizationId) {
+      throw new Error("organization doesn't exist");
+    }
+    const result = await ctx.db.insert("apiKeys", {
+      keyName: args.keyName,
+      provider: "stripe",
+      publicKeyPrefix: args.publicKey.slice(0, 9),
+      secretKeyPrefix: args.secretKey.slice(0, 9),
+      organizationId,
+      userId: auth.subject,
+    });
     return result;
   },
 });
