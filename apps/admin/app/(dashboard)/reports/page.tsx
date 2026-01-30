94755c33be20285e6f0307d470d5065e6649383c
Web <webd75369@gmail.com>
2026-01-30 23:04:30 +0530

    added reports page

---
+++ apps/admin/app/(dashboard)/reports/page.tsx
@@ -0,0 +1,10 @@
+export default function ReportsPage() {
+  return (
+    <div className="px-4 pb-4">
+      <p className="text-lg text-neutral-600 font-light">All Users</p>
+      <p className="text-sm text-muted-foreground font-light">
+        Manage all of the user accounts here
+      </p>
+    </div>
+  );
+}
